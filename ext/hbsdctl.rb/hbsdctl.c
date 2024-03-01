c458d0cadc22683a902e839ddfda4080724ad936
0x1eef <0x1eef@protonmail.com>
2024-03-01 16:51:40 -0300

    s/bsd_eError/get_rb_eError/g

--- ext/hbsdctl.rb/hbsdctl.c
+++ ext/hbsdctl.rb/hbsdctl.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 static VALUE
-bsd_eError(void)
+get_rb_eError(void)
 {
   VALUE rb_mBSD = rb_const_get(rb_cObject, rb_intern("BSD")),
     rb_mControl = rb_const_get(rb_mBSD, rb_intern("Control")),
@@ -52,7 +52,7 @@ feature_set(VALUE self, VALUE path, VALUE state)
   VALUE name;
 
   if (getuid() != 0) {
-    rb_raise(bsd_eError(), "This operation requires root privileges.");
+    rb_raise(get_rb_eError(), "This operation requires root privileges.");
   }
   Check_Type(path, T_STRING);
   Check_Type(state, T_FIXNUM);
@@ -67,7 +67,7 @@ feature_set(VALUE self, VALUE path, VALUE state)
   if (r == 0) {
     return Qtrue;
   } else {
-    rb_raise(bsd_eError(), "hbsdcontrol_set_feature_state failed");
+    rb_raise(get_rb_eError(), "hbsdcontrol_set_feature_state failed");
   }
 }
 
