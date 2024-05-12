ac2428d7c57e691fafcc5a9382e62e05a3937e8e
0x1eef <0x1eef@protonmail.com>
2024-05-12 01:32:20 -0300

    Update readme_examples_test.rb

--- test/readme_examples_test.rb
+++ test/readme_examples_test.rb
@@ -6,10 +6,16 @@ class ReadmeExamplesTest < Test::Unit::TestCase
   require "rbconfig"
   require "test/cmd"
 
-  def test_available_features
-    result = cmd(RbConfig.ruby, readme_example("1_available_features.rb"))
-    assert_equal true, result.status.success?
-    result.each_line { assert_match %r{The [a-zA-Z0-9_]+ feature is available}, _1 }
+  def test_available_features_success
+    assert_equal true,
+                 cmd(RbConfig.ruby, readme_example("1_available_features.rb"))
+                 .status.success?
+  end
+
+  def test_available_features_stdout
+    cmd(RbConfig.ruby, readme_example("1_available_features.rb"))
+    .stdout
+    .each_line { assert_match %r{The [a-zA-Z0-9_]+ feature is available}, _1 }
   end
 
   private
